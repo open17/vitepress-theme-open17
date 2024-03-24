9aa9ee96218937231d105161cae2b77b92676449
open17 <opensci@163.com>
2024-03-24 18:22:56 +0800

    🐎 ci: 更新配置链接

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -49,7 +49,7 @@ export default defineConfig({
       ],
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/open17/vitepress-theme-open17/' }
     ]
   }
 })
