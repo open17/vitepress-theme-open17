3212a642d7d42d02ae91585f065aca67d7b2f447
open17 <opensci@163.com>
2024-03-24 15:10:21 +0800

    ✨ feat: 明暗双背景图

--- docs/.vitepress/theme/components/Blog.vue
+++ docs/.vitepress/theme/components/Blog.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { data as posts } from '../posts.data.js'
 import { useData, withBase } from "vitepress";
-const { frontmatter, theme } = useData()
+const { frontmatter, theme,isDark } = useData()
 const scrollDown = () => {
     window.scrollTo({
         top: window.innerHeight,
@@ -14,7 +14,7 @@ const scrollDown = () => {
     <div class="flex w-full flex-col gpa-5  justify-center items-center">
         <!-- 首页大图 -->
         <div class="w-full h-screen bg-fixed bg-cover bg-center flex justify-center items-center flex-col gap-16 relative"
-            :style="{ 'background-image': `url(${theme.blog.img})` }">
+            :style="{ 'background-image': `url(${isDark?theme.blog.imgDark:theme.blog.img})` }">
             <div class=" text-5xl font-bold">{{ theme.blog.title }}</div>
             <div class=" text-3xl">{{ theme.blog.desc }}</div>
             <!-- 下滑图标 -->
@@ -26,17 +26,18 @@ const scrollDown = () => {
         </div>
         <!-- 博客文章 -->
         <div class="flex mt-20 justify-center items-center gap-5 flex-col w-full">
-            <a :href="withBase(post.url)" class="w-full flex justify-center items-center" v-for="post of posts">
+            <a :href="withBase(post.url)" class="w-full flex justify-center items-center relative"
+                v-for="post of posts">
                 <div
                     class="flex justify-center items-start border-2 rounded-lg min-h-32 w-7/12 flex-col gap-4 px-16 py-6">
-                    <div class="text-2xl">{{ post.frontmatter.title }}</div>
+                    <div class="text-2xl hover:underline underline-offset-4">{{ post.frontmatter.title }}</div>
                     <div v-html="post.excerpt"></div>
                     <div class="flex justify-between w-full items-center">
                         <div>{{ post.frontmatter.date.substring(0, 10) }}</div>
                         <div class="flex justify-end items-end gap-2">
-                            <span class="cursor-pointer hover:underline underline-offset-4 text-[var(--vp-c-indigo-1)]"
-                                v-for="(tag, idx) in post.frontmatter.tags" @click="ActiveTag = tag">{{
-                idx === post.frontmatter.tags.length - 1 ? tag : tag + ',' }}</span>
+                            <a class="cursor-pointer text-[var(--vp-c-indigo-1)]"
+                                v-for="(tag, idx) in post.frontmatter.tags">{{
+                idx === post.frontmatter.tags.length - 1 ? tag : tag + ',' }}</a>
                         </div>
                     </div>
                 </div>
