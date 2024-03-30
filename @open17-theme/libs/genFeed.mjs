028c13ec80ee2f6c29586d09e17e69f368973402
open17 <opensci@163.com>
2024-03-30 21:16:29 +0800

    ✨ feat: RSS

---
+++ @open17-theme/libs/genFeed.mjs
@@ -0,0 +1,45 @@
+import path from 'path'
+import { writeFileSync } from 'fs'
+import { Feed } from 'feed'
+import { createContentLoader} from 'vitepress'
+
+export async function genFeed(config) {
+  const feed = new Feed({
+    title: `${config.site.title}`,
+    description: `${config.site.description}`,
+    id: `${config.site.themeConfig.feed.baseUrl}`,
+    link: `${config.site.themeConfig.feed.baseUrl}`,
+    language: `${config.site.lang}`,
+    image: `${config.site.themeConfig.feed.image}`,
+    favicon: `${config.site.themeConfig.feed.favicon}`,
+    copyright: `${config.site.themeConfig.feed.copyright}`
+  })
+
+  const posts = await createContentLoader('posts/**/*.md', {
+    excerpt: true,
+    render: true
+  }).load()
+
+  posts.sort(
+    (a, b) =>
+      +new Date(b.frontmatter.date) -
+      +new Date(a.frontmatter.date)
+  )
+  for (const { url, excerpt, frontmatter, html } of posts) {
+    feed.addItem({
+      title: frontmatter.title,
+      id: `${config.site.themeConfig.feed.baseUrl}${url}`,
+      link: `${config.site.themeConfig.feed.baseUrl}${url}`,
+      description: excerpt,
+      content: html?.replaceAll('&ZeroWidthSpace;', ''),
+      author: [
+        {
+          name: frontmatter.author
+        }
+      ],
+      date: frontmatter.date
+    })
+  }
+
+  writeFileSync(path.join(config.outDir, 'feed.rss'), feed.rss2())
+}
