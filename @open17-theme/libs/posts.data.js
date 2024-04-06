7458a1e0a989874c2f7bb59906f066b699cc8002
open17 <opensci@163.com>
2024-04-06 13:36:57 +0800

    🐞 fix: 修复引入文件错误

--- @open17-theme/libs/posts.data.js
+++ @open17-theme/libs/posts.data.js
@@ -2,26 +2,28 @@
 import { createContentLoader } from 'vitepress'
 
 export default createContentLoader('posts/**/*.md', {
-  includeSrc: true, // 包含原始 markdown 源?
-  render: true,     // 包含渲染的整页 HTML?
+  includeSrc: false, // 包含原始 markdown 源?
+  render: false,     // 包含渲染的整页 HTML?
   excerpt: true,    // 包含摘录?
   transform(rawData) {
-    // 根据需要对原始数据进行 map、sort 或 filter
-    // 最终的结果是将发送给客户端的内容
-    return rawData.sort((a, b) => {
+    return rawData.map((page) => {
+      const mergedFrontmatter = Object.assign({ title:'Default Title',date: '1900-01-01' }, page.frontmatter);
+      return {
+        frontmatter: mergedFrontmatter,
+        excerpt: page.excerpt,
+        url: page.url,
+      };
+    }).sort((a, b) => {
+      // 在排序时，如果日期为默认日期，则将其放在最前面
       if (a.frontmatter.pin && !b.frontmatter.pin) {
         return -1;
       }
       if (!a.frontmatter.pin && b.frontmatter.pin) {
         return 1;
       }
-      return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date) 
-    }).map((page) => {
-      return {
-        frontmatter: page.frontmatter,
-        excerpt: page.excerpt,
-        url:page.url
-      }
-    })
+      // 否则按日期排序
+      return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date)
+    });
   }
-})
+}
+)
