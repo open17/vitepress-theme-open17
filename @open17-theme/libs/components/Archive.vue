82da251d09e698da1e0f26dab49bfa24a64b5ef1
open17 <opensci@163.com>
2024-07-23 13:32:44 +0800

    fix: 归档页排序错误

--- @open17-theme/libs/components/Archive.vue
+++ @open17-theme/libs/components/Archive.vue
@@ -7,12 +7,12 @@
       <div class="flex mt-10 justify-center items-center">
         <ul>
           <li v-for="year of years">
-            <h2 class="text-3xl my-10 font-bold">{{ year }}</h2>
+            <h2 class="text-3xl my-10 font-bold text-center">{{ year }}</h2>
             <ul>
               <li v-for="(monthPosts, month) in grouped[year]" :key="month">
-                <h3 class="text-xl my-5 font-bold">{{ month }}</h3>
+                <h3 class="text-xl my-5 font-bold text-[--vp-c-brand-3]">{{ monthPosts.monthName }}</h3>
                 <ul>
-                  <li v-for="post in monthPosts" :key="post.id">
+                  <li v-for="post in monthPosts.posts" :key="post.id">
                     <div class="flex items-center gap-10 justify-between mb-3">
                       <a :href="withBase(post.url)" class="mr-2 hover:underline">{{ post.frontmatter.title }}</a>
                       <span class="text-[var(--vp-c-text-3)]">{{ post.frontmatter.date.substring(0, 10) }}</span>
@@ -36,29 +36,32 @@ import BlogLayout from './BlogLayout.vue';
 const { frontmatter, theme } = useData();
 
 const grouped = {};
-
-
-const sortedPosts = posts.sort((a, b) => {
+const sortedPosts = [...posts].sort((a, b) => {
   const dateA = new Date(a.frontmatter.date);
   const dateB = new Date(b.frontmatter.date);
-  return - dateA + dateB;
+  return dateB - dateA;
 });
 
-const years=[];
+
+const years = [];
 
 sortedPosts.forEach(post => {
   const date = new Date(post.frontmatter.date);
   const year = date.getFullYear();
-  const month = date.toLocaleString('default', { month: 'long' });
-  
+  const monthNumber = date.getMonth();
+  const monthName = date.toLocaleString('default', { month: 'long' });
+
   if (!grouped[year]) {
     years.push(year);
     grouped[year] = {};
   }
-  if (!grouped[year][month]) {
-    grouped[year][month] = [];
+  if (!grouped[year][monthNumber]) {
+    grouped[year][monthNumber] = {
+      monthName: monthName,
+      posts: []
+    };
   }
-  grouped[year][month].push(post);
+  grouped[year][monthNumber].posts.push(post);
 });
 
 </script>
